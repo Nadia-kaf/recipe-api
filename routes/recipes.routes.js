bb436f8449ba7f08f4155ae90fe50ecf95d6edb4
Nadia-kaf <nadiabokordedzi@gmail>
2024-04-12 20:20:18 +0000

    login ins and ups

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -1,13 +1,27 @@
 import { Router } from "express";
 import { addRecipe, deleteRecipe, getRecipe, getRecipes, updateRecipe } from "../controllers/recipes.controller.js";
 import multer from "multer";
+import { MulterSaveFilesOrgStorage } from "multer-savefilesorg-storage";
+import dotenv from "dotenv";
+
+
+
+//load env variables
+dotenv.config()
 
 //configure upload middleware
-const upload = multer({dest: 'uploads/images'});
-
+// const upload = multer({dest: 'uploads/images'});
+const upload = multer({
+    storage:MulterSaveFilesOrgStorage({
+        serverPath: `https://savefiles.org/api/v1/uploads`,
+        apiAccessToken: process.env.SAVEFILESORG_API_KEY,
+        relativePath: '/recipes/images/*'
+    }),
+    preservePath: true
+});
 
 //Create Recipes router
-const router =Router();
+const router = Router();
 
 router.post('/', upload.single('image'),addRecipe);
 
